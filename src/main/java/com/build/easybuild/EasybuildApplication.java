b18678f17a4b38d0e9ac2f13bbec16f7e6b3bbb3
e <2446802702@qq.com>
2018-05-07 14:34:44 +0800

    初始化项目

---
+++ src/main/java/com/build/easybuild/EasybuildApplication.java
@@ -0,0 +1,12 @@
+package com.build.easybuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasybuildApplication.class, args);
+    }
+}
