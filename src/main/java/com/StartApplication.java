8820adf6c727b1833c0f6c4987973ccb3c46e701
e <2446802702@qq.com>
2018-05-07 17:00:40 +0800

    添加配置文件

---
+++ src/main/java/com/StartApplication.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot启动类
+ */
+@MapperScan("com.easybuild.cores.dao")
+@SpringBootApplication(scanBasePackages = "com")
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
