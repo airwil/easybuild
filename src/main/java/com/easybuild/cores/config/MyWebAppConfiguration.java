10bddc157fe2f81dbc6a3f7053756156d7568369
e <2446802702@qq.com>
2018-05-22 16:16:31 +0800

    添加静态文件

--- src/main/java/com/easybuild/cores/config/MyWebAppConfiguration.java
+++ src/main/java/com/easybuild/cores/config/MyWebAppConfiguration.java
@@ -1,14 +1,18 @@
 package com.easybuild.cores.config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+/**
+ * SpringBoot个性化配置
+ */
 @SuppressWarnings("deprecation")
 @Configuration
 public class MyWebAppConfiguration extends WebMvcConfigurerAdapter{
 	//读取配置文件
-//	@Value(value = "${localFile}")
-//	private String localFile;
+	@Value(value = "${custom.uploadPath}")
+	private String uploadPath;
 	
 	/**
 	 * 设置SpringBoot读取本地文件，相当于虚拟一个服务器
@@ -17,7 +21,7 @@ public class MyWebAppConfiguration extends WebMvcConfigurerAdapter{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		
 		//指定“/pic/**”定向到本地路径“F:/testUpload/”
-		registry.addResourceHandler("/pic/**").addResourceLocations("file:F:/testUpload/");
+		registry.addResourceHandler("/pic/**").addResourceLocations("file:"+uploadPath);
 		super.addResourceHandlers(registry);
 	}
 }
