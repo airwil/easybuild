4a28634718c4a3d9db95503a617b0b10bce2ea15
e <2446802702@qq.com>
2018-05-25 09:10:34 +0800

    添加serviceImpl模板

--- src/main/resources/templates/Service.java
+++ src/main/resources/templates/Service.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.Map;
+
 package [[${packages}]]
 [# th:each="item : ${imports}"]
 import [(${item})];\n
@@ -7,6 +10,40 @@ import [(${item})];\n
  * [[${notes}]]
  * 
  */
-public interface [[${className}]]Service extends BaseDao<[[${className}]]>{
+public interface [[${className}]]Service {
+	/**
+	 * 插入
+	 */
+	int insert([[${className}]] record);
+    
+	
+	/**
+	 * 根据主键删除
+	 */
+	int deleteByPrimaryKey(String id);
+	 
+	
+	/**
+	 * 根据主键更新
+	 */
+	int updateByPrimaryKey([[${className}]] record);
 
+	
+	/**
+	 * 根据主键查询
+	 */
+	[[${className}]] selectByPrimaryKey(String id);
+
+	
+	/**
+	 * 分页、条件、排序查询
+	 */
+	List<[[${className}]]> select(Map<String,Object> map);
+	
+	
+	/**
+	 * 根据条件计算总数据量
+	 */
+	int count(Map<String,Object> map);
+	
 }
---
+++ src/main/resources/templates/ServiceImpl.java
@@ -0,0 +1,67 @@
+package [[${packages}]]
+
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+[# th:each="item : ${imports}"]
+import [(${item})];\n
+[/]
+
+/**
+ * [[${notes}]]
+ * 
+ */
+@Service
+public class [[${className}]]ServiceImpl {
+	@Autowired
+	private [[${className}]]Dao [[${className2}]]Dao;
+	
+	/**
+	 * 插入
+	 */
+	public int insert([[${className}]] record) {
+		return [[${className2}]]Dao.insert(record);
+	}
+    
+	
+	/**
+	 * 根据主键删除
+	 */
+	public int deleteByPrimaryKey(String id) {
+		return [[${className2}]]Dao.deleteByPrimaryKey(id);
+	}
+	 
+	
+	/**
+	 * 根据主键更新
+	 */
+	public int updateByPrimaryKey([[${className}]] record) {
+		return [[${className2}]]Dao.updateByPrimaryKey(record);
+	}
+
+	
+	/**
+	 * 根据主键查询
+	 */
+	public [[${className}]] selectByPrimaryKey(String id){
+		return [[${className2}]]Dao.selectByPrimaryKey(id);
+	}
+
+	
+	/**
+	 * 分页、条件、排序查询
+	 */
+	public List<[[${className}]]> select(Map<String,Object> map){
+		return [[${className2}]]Dao.select(map);
+	}
+	
+	
+	/**
+	 * 根据条件计算总数据量
+	 */
+	public int count(Map<String,Object> map) {
+		return [[${className2}]]Dao.count(map);
+	}
+	
+}
